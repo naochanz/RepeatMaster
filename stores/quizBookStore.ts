91ce2fae0ceab52c12d653773cfeaaaecba97248
naochanz <naoki.92700@gmail.com>
2025-12-03 22:48:41 +0900

    Remove commented-out file header from quizBookStore.ts

--- stores/quizBookStore.ts
+++ stores/quizBookStore.ts
@@ -1,4 +1,3 @@
-// stores/quizBookStore.ts
 import { create } from 'zustand';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 import { mockQuizBooks } from '@/mockData/mockQuizBooks';
